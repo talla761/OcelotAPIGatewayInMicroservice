86abec94db4457b839b5b497a3c4bfed7f8ea675
talla761 <yvantalla07@gmail.com>
2025-06-02 22:02:45 -0400

    Add project files.

---
+++ Identity/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static readonly string[] Users = new[]
+        {
+            "Yvan", "Talla", "LeDoux"
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Users);
+        }
+    }
+}
